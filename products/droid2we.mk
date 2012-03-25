8fcf183e92ae0a86d65bffa444eb7573874fb3cd
Dustin Einglett <x13thangelx@gmail.com>
2012-03-25 13:15:37 -0500

    Remove unneeded devices and add mine

---
+++ products/droid2we.mk
@@ -0,0 +1,22 @@
+# Inherit AOSP device configuration for inc.
+$(call inherit-product, device/motorola/droid2we/droid2we.mk)
+
+# Inherit some common aokp stuff.
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := aokp_droid2we
+PRODUCT_BRAND := verizon
+PRODUCT_DEVICE := droid2we
+PRODUCT_MODEL := DROID2 Global
+PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_PACKAGES += \
+    Camera
+
+# boot animation
+PRODUCT_COPY_FILES += \
+	vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
--- products/shadow.mk
+++ products/shadow.mk
@@ -1,20 +1,22 @@
 # Inherit AOSP device configuration for inc.
-$(call inherit-product, device/htc/bravo/bravo.mk)
+$(call inherit-product, device/motorola/shadow/shadow.mk)
 
 # Inherit some common aokp stuff.
 $(call inherit-product, vendor/aokp/configs/common_phone.mk)
 
+
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := aokp_bravo
-PRODUCT_BRAND := htc
-PRODUCT_DEVICE := bravo
-PRODUCT_MODEL := Desire
-PRODUCT_MANUFACTURER := HTC
+PRODUCT_NAME := aokp_shadow
+PRODUCT_BRAND := verizon
+PRODUCT_DEVICE := shadow
+PRODUCT_MODEL := DROIDX
+PRODUCT_MANUFACTURER := Motorola
 
 PRODUCT_PACKAGES += \
     Camera
 
+# boot animation
 PRODUCT_COPY_FILES += \
 	vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
