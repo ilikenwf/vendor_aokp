18a6b19f528f82ab894fbf8d15633be0a65a40ee
Matt Parnell <parwok@gmail.com>
2012-06-11 01:06:33 -0500

    remove theme engine

--- configs/common.mk
+++ configs/common.mk
@@ -1,9 +1,6 @@
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/common
 
-# T-Mobile theme engine
-include vendor/aokp/configs/themes_common.mk
-
 PRODUCT_PACKAGES += \
     ROMControl \
     su \
