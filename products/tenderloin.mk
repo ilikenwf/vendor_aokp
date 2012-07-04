9449dcb1699aa676366c5df11057ec82188f6b4c
Matt Parnell <parwok@gmail.com>
2012-07-04 01:25:34 -0500

    try using an up to date fingerprint and spoof as maguro

--- products/tenderloin.mk
+++ products/tenderloin.mk
@@ -6,10 +6,12 @@ $(call inherit-product, vendor/aokp/configs/common_tablet.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/tenderloin
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_ID=IML74K BUILD_FINGERPRINT=google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys" BUILD_NUMBER=189904
-
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=touchpad BUILD_FINGERPRINT=google/yakju/maguro:4.0.4/IMM76D/299849:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.0.4 IMM76D 299849 release-keys" BUILD_NUMBER=299849
 PRODUCT_NAME := HP_Touchpad
-PRODUCT_DEVICE := tenderloin
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := maguro
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := samsung
 
 # boot animation
 PRODUCT_COPY_FILES += \
